ddd32442653d77a47def286aba5894dbd02fc2fc
RR1335 <odyli@126.com>
2025-02-16 00:53:52 -0500

    Update Blog handleBlogRouter Function

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const serverHandle = (req,res) => {
     res.setHeader('Content-type','application/json')
 
-    const resData = {
-        name: 'RR1335',
-        site: 'Node.js Beginning',
-        env: process.env.NODE_ENV
-    }
 
-    res.end(
-        JSON.stringify(resData)
-    )
+
+
+
+
+// 测试的数据，初次建立测试 server 创建成功
+    // const resData = {
+    //     name: 'RR1335',
+    //     site: 'Node.js Beginning',
+    //     env: process.env.NODE_ENV
+    // }
+
+    // res.end(
+    //     JSON.stringify(resData)
+    // )
 }
 
 
