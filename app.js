c8691f356f9f8f6e428051cac50328eee8f98bea
RR1335 <odyli@126.com>
2025-02-16 01:12:06 -0500

    Update add Router ,App.js

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
+const handleBlogRouter = require('./src/router/blog')
+const handleUserRouter = require('./src/router/user')
+
+
 const serverHandle = (req,res) => {
     res.setHeader('Content-type','application/json')
 
+    const blogData = handleBlogRouter(req,res)
+    if (blogData) {
+        res.end (
+            JSON.stringify(blogData)
+        )
+        return
+    }
+
+    const userData = handleUserRouter(req,res)
+    if (userData) {
+        res.end (
+            JSON.stringify(userData)
+        )
+        return
+    }
 
 
 
-
+    // 没有对应的路由匹配，则返回 404
+    res.writeHead(404,{"Content-type":"text/plain"})
+    res.write("404 Not Found\n")
+    res.end()
 
 // 测试的数据，初次建立测试 server 创建成功
     // const resData = {
