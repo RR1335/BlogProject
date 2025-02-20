2fd03305d25b32be211b54a316dddd5985cb5627
RR1335 <odyli@126.com>
2025-02-20 08:00:45 -0500

    Update Crypto

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,10 +1,14 @@
 const {exec, escape } = require('../db/mysql')
+const { genPassword } = require('../utils/crypto')
 
 
 const signin = (username,password) => {
     // 防止SQL注入
     username = escape(username)
     password = escape(password)
+
+    // 生成加密密码
+    password = genPassword(password)
 
     // 去掉 username=‘${username}’ and password=’${password}‘ 去掉单引号
     const sql = `
